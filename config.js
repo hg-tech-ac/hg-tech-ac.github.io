613f36a7aa0eda9896a8840d0a3036426b725a51
hg-tech-ac <jjiuk.eum@gmail.com>
2026-08-04 14:20:56 +0900

    Fix Apps Script deployment ID casing

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.HG_CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbxAYYpPLK9fLlpGNKfj9xoE021IBhv38ESNyflls_kAwR_xT7y-U6veNFZ4-_eB3Eyo/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxAYYpPLK9fLIpGNKfj9xoE021lBhv38ESNyfIls_kAwR_xT7y-U6veNFZ4-_eB3Eyo/exec",
   SESSION_KEY: "hgTechAcAdminSession",
   DEVICES: Object.freeze([
     { deviceId: "TECH_1", name: "기술실 1번" },
