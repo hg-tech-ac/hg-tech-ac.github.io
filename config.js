fb7db63ede8b7216fccb87a5366ea9f04a52b7cd
hg-tech-ac <jjiuk.eum@gmail.com>
2026-08-04 14:35:33 +0900

    Map logical AC devices to two ESP32 controllers

--- config.js
+++ config.js
@@ -2,8 +2,23 @@ window.HG_CONFIG = Object.freeze({
   API_URL: "https://script.google.com/macros/s/AKfycbxAYYpPLK9fLIpGNKfj9xoE021lBhv38ESNyfIls_kAwR_xT7y-U6veNFZ4-_eB3Eyo/exec",
   SESSION_KEY: "hgTechAcAdminSession",
   DEVICES: Object.freeze([
-    { deviceId: "TECH_1", name: "기술실 1번" },
-    { deviceId: "TECH_2", name: "기술실 2번" },
-    { deviceId: "TECH_3D", name: "3D 작업실" }
+    {
+      deviceId: "TECH_1",
+      name: "기술실 1번",
+      controllerId: "TECH_ROOM",
+      servoChannel: 1
+    },
+    {
+      deviceId: "TECH_2",
+      name: "기술실 2번",
+      controllerId: "TECH_ROOM",
+      servoChannel: 2
+    },
+    {
+      deviceId: "TECH_3D",
+      name: "3D 작업실",
+      controllerId: "TECH_3D_ROOM",
+      servoChannel: 1
+    }
   ])
 });
