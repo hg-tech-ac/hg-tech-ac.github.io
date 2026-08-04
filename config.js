c572a6ccc978a1698a5f86a7303e190abc243f4c
hg-tech-ac <jjiuk.eum@gmail.com>
2026-08-04 11:53:15 +0900

    Configure admin session and devices

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-/*
- * Google Apps Script 웹 앱 설정
- * Deployment ID:
- * AKfycbxAYYpPLK9fLlpGNKfj9xoE021IBhv38ESNyflls_kAwR_xT7y-U6veNFZ4-_eB3Eyo
- */
-window.HG_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxAYYpPLK9fLlpGNKfj9xoE021IBhv38ESNyflls_kAwR_xT7y-U6veNFZ4-_eB3Eyo/exec"
-};
+window.HG_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbxAYYpPLK9fLlpGNKfj9xoE021IBhv38ESNyflls_kAwR_xT7y-U6veNFZ4-_eB3Eyo/exec",
+  SESSION_KEY: "hgTechAcAdminSession",
+  DEVICES: Object.freeze([
+    { deviceId: "TECH_1", name: "기술실 1번" },
+    { deviceId: "TECH_2", name: "기술실 2번" },
+    { deviceId: "TECH_3D", name: "3D 작업실" }
+  ])
+});
